9102dcf237b97d4e12eba91d6b83da4edcfb70c0
Sangyoon Lee <pinetree408@gmail.com>
2017-03-08 16:17:28 +0900

    Refactoring prevent logic the way to modulize and make api

--- src/main/java/com/pinetree408/keme/prevent/Prevent.java
+++ src/main/java/com/pinetree408/keme/prevent/Prevent.java
@@ -1,33 +1,17 @@
 package com.pinetree408.keme.prevent;
 
 /** Created by user on 2016-12-28. */
-import org.jnativehook.GlobalScreen;
-import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
-import org.jnativehook.keyboard.NativeKeyListener;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
 import com.pinetree408.keme.util.Util;
-import com.pinetree408.keme.util.ModeErrorLogger;
 
-public class Prevent implements NativeKeyListener {
+public class Prevent {
 
-  static Robot robot;
   static Util util;
-  /** buffer writer to save log */
-  private static ModeErrorLogger meLogger;
-
-  static String prevTopProcess;
-  static String nowTopProcess;
-  static String nowLanguage;
 
   private static int preventState;
   private static final int checking = 0;
@@ -36,26 +20,12 @@ public class Prevent implements NativeKeyListener {
 
   public Prevent() {
 
-    // Initialize Robot for prevent word injection
-    try {
-      robot = new Robot();
-    } catch (AWTException ex) {
-      // TODO Auto-generated catch block
-      ex.printStackTrace();
-    }
-
     util = new Util();
-    meLogger = new ModeErrorLogger("result.txt");
-
-    prevTopProcess = "initial";
-    nowTopProcess = "initial";
-    nowLanguage = "initial";
 
     preventState = checking;
   }
 
-  public void nativeKeyPressed(NativeKeyEvent e) {
-
+  public void keyPressed(NativeKeyEvent e, Robot robot) {
     switch (preventState) {
       case checking:
         break;
@@ -104,72 +74,29 @@ public class Prevent implements NativeKeyListener {
         preventState = checking;
         break;
     }
-    meLogger.log(e, nowLanguage, nowTopProcess, String.valueOf(preventState), "null");
   }
 
-  public static void main(String[] args) {
+  public String getPreventState() {
+    return String.valueOf(preventState);
+  }
 
-    // Set jnativehook logger level to off state
-    Logger EventLogger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
-    EventLogger.setLevel(Level.OFF);
+  public void injection(String nowLanguage, Robot robot) {
+    preventState = prevent;
 
-    try {
-      GlobalScreen.registerNativeHook();
-    } catch (NativeHookException ex) {
-      System.err.println("There was a problem registering the native hook.");
-      System.err.println(ex.getMessage());
-
-      System.exit(1);
+    robot.delay(200);
+    if (nowLanguage.equals("ko")) {
+      robot.keyPress(KeyEvent.VK_G);
+      robot.keyRelease(KeyEvent.VK_G);
+      robot.keyPress(KeyEvent.VK_K);
+      robot.keyRelease(KeyEvent.VK_K);
+      robot.keyPress(KeyEvent.VK_S);
+      robot.keyRelease(KeyEvent.VK_S);
+    } else {
+      robot.keyPress(KeyEvent.VK_E);
+      robot.keyRelease(KeyEvent.VK_E);
+      robot.keyPress(KeyEvent.VK_N);
+      robot.keyRelease(KeyEvent.VK_N);
     }
-
-    GlobalScreen.addNativeKeyListener(new Prevent());
-
-    Timer jobScheduler = new Timer();
-    jobScheduler.schedule(
-        new TimerTask() {
-          @Override
-          public void run() {
-
-            nowTopProcess = util.nowTopProcess();
-
-            if (!nowTopProcess.equals("")) {
-
-              if (!prevTopProcess.equals(nowTopProcess)) {
-
-                preventState = prevent;
-
-                prevTopProcess = util.nowTopProcess();
-
-                nowLanguage = util.nowLanguage();
-
-                robot.delay(200);
-                if (nowLanguage.equals("ko")) {
-                  robot.keyPress(KeyEvent.VK_G);
-                  robot.keyRelease(KeyEvent.VK_G);
-                  robot.keyPress(KeyEvent.VK_K);
-                  robot.keyRelease(KeyEvent.VK_K);
-                  robot.keyPress(KeyEvent.VK_S);
-                  robot.keyRelease(KeyEvent.VK_S);
-                } else {
-                  robot.keyPress(KeyEvent.VK_E);
-                  robot.keyRelease(KeyEvent.VK_E);
-                  robot.keyPress(KeyEvent.VK_N);
-                  robot.keyRelease(KeyEvent.VK_N);
-                }
-                robot.waitForIdle();
-              }
-            }
-          }
-        },
-        0,
-        100);
-  }
-
-  public void nativeKeyReleased(NativeKeyEvent e) {
-    //System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
-  }
-
-  public void nativeKeyTyped(NativeKeyEvent e) {
-    //System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    robot.waitForIdle();
   }
 }
